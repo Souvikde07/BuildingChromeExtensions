f9f437aa537b52082115d7a1d94db67727433def
Souvik De <souvik.de@sato-global.com>
2023-07-07 11:09:13 +0900

    Added template strings into the Js function renderLeads

--- script.js
+++ script.js
@@ -13,7 +13,13 @@ inputBtn.addEventListener("click",function(){
 function renderLeads(){
     let listItems = ""
     for(let i = 0;i<myLeads.length;i++){
-        listItems += "<li><a href = '#' target = '_blank'>" + myLeads[i] + "</a></li>"
+       // listItems += "<li><a href = ' "+myLeads[i]+" 'target = '_blank'>" + myLeads[i] + "</a></li>"
+       listItems += `<li>
+                        <a href = '${myLeads[i]}' target = '_blank'>" +  + 
+                        ${myLeads[i]}
+                        </a>
+                    </li>
+    `
     }
     ulEl.innerHTML = listItems
 }
