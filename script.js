36d0674aed758fe3af8bb35af8f7d08a23683f5c
Souvik De <souvik.de@sato-global.com>
2023-07-07 10:57:45 +0900

    Added the anchor tags to make the strings into links and open them in a different page.

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 const inputBtn = document.getElementById("input-btn")
-let myLeads = ["sdvs","dvgsfdhga","mjsdvlsn"]
+let myLeads = []
 const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
 
 
 inputBtn.addEventListener("click",function(){
     myLeads.push(inputEl.value)
-    console.log(myLeads)
+    renderLeads()
+    inputEl.value = ""
 })
 
-let listItems = ""
-
-for(let i = 0;i<myLeads.length;i++){
-    listItems += "<li>" + myLeads[i] + "</li>" 
+function renderLeads(){
+    let listItems = ""
+    for(let i = 0;i<myLeads.length;i++){
+        listItems += "<li><a href = '#' target = '_blank'>" + myLeads[i] + "</a></li>"
+    }
+    ulEl.innerHTML = listItems
 }
-ulEl.innerHTML = listItems
