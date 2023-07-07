f0e24026f59b8a0a9e181fd4c75431adbdd9a6bc
Souvik De <souvik.de@sato-global.com>
2023-07-07 09:43:08 +0900

    Adding a practice script for Practicing new Javascript for the Project

--- practice.js
+++ practice.js
@@ -1,4 +1,8 @@
-const taga = document.getElementById("div")
+const container = document.getElementById("container")
 
-taga.innerHTML = "<button>BUY!</button>"
-console.log(taga)
+container.innerHTML = "<button id='buyme'>BUY!</button>"
+const id = document.getElementById("buyme")
+id.addEventListener("click",function(){
+    container.innerHTML += "<p>Thank you for Buying!!</p>"
+})
+
