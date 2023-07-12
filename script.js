8cb2b25815b27ae8c2592f3517c6e534be6d7466
Souvik De <souvik.de@sato-global.com>
2023-07-12 14:05:12 +0900

    Feat: Save the hard coded tab to the localStorage on clicking the save button

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const inputBtn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
 const deletebtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
 if(leadsFromLocalStorage){
@@ -10,9 +11,19 @@ if(leadsFromLocalStorage){
     render(myLeads)
 }
 
+const tabs = [
+    {url: "http://www.linkedin.com/in/per-harald-borgen/"},
+]
+
+tabBtn.addEventListener("click", function(){
+    myLeads.push(tabs[0].url)
+    localStorage.setItem("myLeads",JSON.stringify(myLeads))
+    render(myLeads)
+})
+
 function render(leads){
     let listItems = ""
-    for(let i = 0;i<Leads.length;i++){
+    for(let i = 0;i<leads.length;i++){
        // listItems += "<li><a href = ' "+myLeads[i]+" 'target = '_blank'>" + myLeads[i] + "</a></li>"
        listItems += `
                     <li>
